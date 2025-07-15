8cc8f32be575506907370b099bddff3b9a804868
Rahmad2830 <rahmatnurhidayat0101@gmail.com>
2025-07-15 11:13:05 +0700

    add typing animation and change some component

--- src/script/script.js
+++ src/script/script.js
@@ -5,4 +5,30 @@ export default (Alpine) => {
       this.isOpen = !this.isOpen
     }
   }))
+  Alpine.data("typing", () => ({
+    fullText: "Developer",
+    text: '',
+    index: 0,
+    init() {
+      this.type()
+    },
+    type() {
+      if(this.index < this.fullText.length) {
+        this.text += this.fullText[this.index]
+        this.index++
+        setTimeout(() => this.type(), 150)
+      } else {
+        setTimeout(() => this.erase(), 2000)
+      }
+    },
+    erase() {
+      if(this.index > 0) {
+        this.text = this.text.slice(0, -1)
+        this.index--
+        setTimeout(() => this.erase(), 150)
+      } else {
+        setTimeout(() => this.type(), 1000)
+      }
+    }
+  }))
 }
